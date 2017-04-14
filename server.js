23fd6569b88c6e8e3c7143d654f9cbf4cd2ee096
Eric Arbour <arboureric@gmail.com>
2017-04-14 22:41:18 +0000

    can retrieve existing urls

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var port = 8080;
 var MongoClient = require('mongodb').MongoClient;
-var testDb = 'mongodb://localhost:27017/test'
+var testDb = 'mongodb://localhost:27017/test';
+var thisUrl = 'http://www.some-heroku-thing.com/';
 
 MongoClient.connect(testDb, function(err, db) {
  if (err) {
@@ -13,18 +14,21 @@ MongoClient.connect(testDb, function(err, db) {
    res.send('Hey, you either want a url shortened or redirected to a rul, right?');
   });
   app.get('/:url', function(req, res) {
-   var cursor = db.collection('urls').find().toArray(function(err, items) {
+   var cursor = db.collection('urls').findOne({short_url: req.params.url}, function(err, doc) {
     if (err) {
      throw err; 
     } else {
-     items.forEach(function(item) {
-      if (item.hasOwnProperty(req.params.url)) {
-       console.log(item[req.params.url]);
-      }
-     });
+     if (doc) {
+      var obj = {"original_url": "", "short_url": thisUrl};
+      obj.short_url += doc.short_url;
+      obj.original_url += doc.original_url;
+      res.send(obj);
+     } else {
+      console.log(doc);
+      res.send(doc); 
+     }
     }
    }); 
-   res.send(req.params.url);
   }); 	
  }
 });
