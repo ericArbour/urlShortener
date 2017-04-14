e979b4fea06f15fc07efe1fee958b69ba4604238
Eric Arbour <arboureric@gmail.com>
2017-04-14 19:02:28 +0000

    can access database

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var app = express();
 var port = 8080;
+var MongoClient = require('mongodb').MongoClient;
+var testDb = 'mongodb://localhost:27017/test'
 
-app.get('/', function(req, res) {
- res.send('Hello, Newman');	
+MongoClient.connect(testDb, function(err, db) {
+ if (err) {
+  throw err;
+ } else { 
+  console.log('connected to db: ' + testDb);
+  app.get('/', function(req, res) {
+   res.send('Hey, you either want a url shortened or redirected to a rul, right?');
+  });
+  app.get('/:url', function(req, res) {
+   var cursor = db.collection('urls').find().toArray(function(err, items) {
+    if (err) {
+     throw err; 
+    } else {
+     items.forEach(function(item) {
+      if (item.hasOwnProperty(req.params.url)) {
+       console.log(item[req.params.url]);
+      }
+     });
+    }
+   }); 
+   res.send(req.params.url);
+  }); 	
+ }
 });
 
 app.listen(process.env.PORT || port, function() {
