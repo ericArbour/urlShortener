a5999d4f82e1078f6583be4387585b785b5bddfb
Eric Arbour <arboureric@gmail.com>
2017-04-16 15:18:55 +0000

    updated for heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var port = 8080;
 var MongoClient = require('mongodb').MongoClient;
-var testDb = 'mongodb://localhost:27017/test';
-var thisUrl = 'http://www.shorturl.com/';
+var testDb = process.env.MONGOLAB_URI;
+var thisUrl = 'https://infinite-everglades-36931.herokuapp.com/';
 var urlRegex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i
 
 MongoClient.connect(testDb, function(err, db) {
