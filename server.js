effbf93c3c4e1e245287b0c3df5c25e6c105e3de
Eric Arbour <arboureric@gmail.com>
2017-04-16 14:37:30 +0000

    complete, but could be refactored

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var port = 8080;
 var MongoClient = require('mongodb').MongoClient;
 var testDb = 'mongodb://localhost:27017/test';
-var thisUrl = 'http://www.some-heroku-thing.com/';
+var thisUrl = 'http://www.shorturl.com/';
 var urlRegex = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i
 
 MongoClient.connect(testDb, function(err, db) {
@@ -44,7 +44,7 @@ MongoClient.connect(testDb, function(err, db) {
            //if short url or original url wasn't found, create new record if it is a valid url
            if (urlRegex.test(req.params.url + req.params[0])) {
             console.log('valid url');
-            db.collection('urls').insertOne({"original_url": req.params.url + req.params[0], "short_url": String(items.length - 1)}, function(err, result) {
+            db.collection('urls').insertOne({"original_url": req.params.url + req.params[0], "short_url": String(items.length)}, function(err, result) {
              if (err) {
               throw err;
              } else {
